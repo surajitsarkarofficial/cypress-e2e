0ff561e33c24a50bd52d548523ee7a48239f18c1
Surajit Sarkar <suro@Surajits-MacBook-Pro-2.local>
2023-12-21 20:25:20 +0530

    iframes

---
+++ cypress/e2e/16-iframes.cy.ts
@@ -0,0 +1,25 @@
+describe('Handle IFrames', () => {
+    it('iFrame', () => {
+        cy.visit("https://the-internet.herokuapp.com/iframe")
+        cy.get('#mce_0_ifr').then(($frame)=>{
+            const body = $frame.contents().find('body')
+            cy.wrap(body).find('p').clear()
+            cy.wrap(body).type("Typing from Cypress")
+        })
+    });
+    it.only('More iFrame', () => {
+        cy.visit("https://automationtesting.co.uk/iframes.html")
+        cy.get('iframe').then(($frame)=>{
+            const innerFrame = $frame.contents().find('body')
+            cy.wrap(innerFrame).find('.toggle').click()
+            cy.wrap(innerFrame).find('#menu li>a').each((menu)=>{
+                if(menu.text()==='Actions')
+                {
+                    cy.wrap(menu).click();
+                   
+                }
+                cy.wrap(innerFrame).contains("Actions").should('be.visible')
+            })
+        })
+    });
+});
